e62013f40e574a546b3d4d9b1c68de2240f27be0
Angular CLI <angular-cli@angular.io>
2017-07-16 16:29:20 +0200

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { WeatherForecastPage } from './app.po';
+
+describe('weather-forecast App', () => {
+  let page: WeatherForecastPage;
+
+  beforeEach(() => {
+    page = new WeatherForecastPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
