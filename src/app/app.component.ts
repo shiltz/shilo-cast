d9b012cb23f294707b68caae462508f20e59906b
Shilton Naicker <shilton.naicker@standardbank.co.za>
2017-07-19 20:14:01 +0200

    draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import { OnInit } from '@angular/core';
+import {Hero} from "./hero";
+import { HeroService } from './special-super-hero.service';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    providers: [HeroService]
 })
-export class AppComponent {
-  title = 'My first angular app';
+export class AppComponent implements OnInit {
+    title = 'Tour of heroes';
+    selectedHero: Hero;
+    heroes : Hero[];
+
+    constructor(private heroService: HeroService) { }
+
+    ngOnInit(): void {
+        this.getHeroes();
+    }
+
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
+
+    getHeroes(): void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
 }
